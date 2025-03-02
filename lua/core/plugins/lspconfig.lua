6dc730bc060344309878092bd3abfca89ffa438a
Daiwa Zou <daiwa.zou@outlook.com>
2025-03-01 19:01:47 -0500

    Update lspconfig

--- lua/core/plugins/lspconfig.lua
+++ lua/core/plugins/lspconfig.lua
@@ -97,8 +97,8 @@ return {
           },
         },
       },
-    gopls = {},
-    ruff-lsp = {},
+      gopls = {},
+      rust_analyzer = {},
     }
     require('mason').setup()
     local ensure_installed = vim.tbl_keys(servers or {})
@@ -107,7 +107,7 @@ return {
       'stylua',
       'black',
       'isort',
-      'jq', 
+      'jq',
       'gofumpt',
       'rustfmt',
       -- Linter
@@ -122,9 +122,6 @@ return {
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
